0020102e21849f4c02ab3315e572f453d8cddeb3
Chanachai04 <nexus87th@gmail.com>
2025-01-16 14:59:34 +0700

    v.2

---
+++ lib/views/welcome_ui.dart
@@ -0,0 +1,95 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class WelcomUI extends StatefulWidget {
+  const WelcomUI({super.key});
+
+  @override
+  State<WelcomUI> createState() => _WelcomUIState();
+}
+
+class _WelcomUIState extends State<WelcomUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.2,
+            ),
+            Image.asset(
+              "assets/images/logo.png",
+              width: MediaQuery.of(context).size.width * 0.65,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+            ),
+            Text(
+              "DTI SAU APP 2025",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.03,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Southeast Asia University",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Created by Chanachai DTI SAU",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    fixedSize: Size(MediaQuery.of(context).size.width * 0.3,
+                        MediaQuery.of(context).size.height * 0.05),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.05,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    fixedSize: Size(MediaQuery.of(context).size.width * 0.3,
+                        MediaQuery.of(context).size.height * 0.05),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
